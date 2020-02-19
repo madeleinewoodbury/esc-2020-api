9b24d3034f4e48149ff3fe723f335000f4ca0507
madeleinewoodbury <randine25@gmail.com>
2020-02-19 12:50:41 -0600

    Initial commit. Server and database setup

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const colors = require('colors');
+const connectDB = require('./config/db');
+
+// Load env vars
+dotenv.config({ path: './config/config.env' });
+
+// Connect to DB
+connectDB();
+
+const app = express();
+
+// Body parser
+app.use(express.json());
+
+app.get('/', (req, res) => res.send('Api Running...'));
+
+// Dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+const PORT = process.env.PORT;
+const server = app.listen(PORT, () => {
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.magenta
+      .bold
+  );
+});
+
+// Handle unhandled promise rejection
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`.red.bold);
+  server.close(() => process.exit(1));
+});
